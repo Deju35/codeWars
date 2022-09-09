a79ea6c6bd823a8b395df3955da9be69e7a6ce8c
Deju35 <107037555+Deju35@users.noreply.github.com>
2022-09-09 14:06:19 +0300

    Create Get the Middle Character.js

---
+++ 7ku/Count of positives/Get the Middle Character.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+
+function getMiddle(s){
+  let length = s.length
+  let middle = Math.floor(length/2)
+  
+  if(length%2 === 0){  /// if list is even
+    return s[middle-1] + s[middle]
+  }else{// if odd
+    return s[middle]
+  }
+  
+  
+  
+}
