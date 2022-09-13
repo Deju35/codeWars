d843e0c192cebb6404bbfc4ce72a26500d3dcff3
Deju35 <107037555+Deju35@users.noreply.github.com>
2022-09-13 13:04:49 +0300

    Create Coding Meetup #14 - Higher-Order Functions Series - Order the food.js

---
+++ 7ku/solutions/Coding Meetup #14 - Higher-Order Functions Series - Order the food.js
@@ -0,0 +1,29 @@
+
+// DESCRIPTION:
+// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an object which includes the count of food options selected by the developers on the meetup sign-up form..
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+//     meal: 'vegetarian' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+//     meal: 'standard' },
+//   { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+//     meal: 'vegan' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+//     meal: 'vegetarian' },
+// ];
+function orderFood(list) {
+  let hash = {}
+  for(const elem of list){
+    if(hash[elem.meal]){
+      hash[elem.meal] +=1
+    }else{
+      hash[elem.meal] = 1
+    }
+  }
+  return hash
+}
