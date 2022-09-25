ea6e134a3fc7fb84d3b0770e8658726b24ee6fa5
Deju35 <107037555+Deju35@users.noreply.github.com>
2022-09-25 20:42:07 +0300

    Update Find the unique Number.js

--- 6ku/Find the unique Number.js
+++ 6ku/Find the unique Number.js
@@ -1,5 +1,5 @@
 // DESCRIPTION:
-// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// There is an array with some numbers. All numbers are equal except for one. Try finding it!
 
 // findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
 // findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
