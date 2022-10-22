a4b8bce865876e566c39fd6a8fa2297ee8eb9d69
Dejen Tesfai <107037555+Deju35@users.noreply.github.com>
2022-10-22 14:02:59 +0300

    Update Even numbers in an array.js

--- 7ku/solutions/Even numbers in an array.js
+++ 7ku/solutions/Even numbers in an array.js
@@ -5,3 +5,12 @@
 // ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+
+function evenNumbers(array, number) {
+  let res = array.filter((x)=>x%2==0)
+  return res.splice(res.length-number, number)
+    
+  
+
+}
