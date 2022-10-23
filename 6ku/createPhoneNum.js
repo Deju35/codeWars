d4821693bfaae2b341b252c5d3c349bc104d37ca
Dejen Tesfai <107037555+Deju35@users.noreply.github.com>
2022-10-23 15:57:26 +0300

    Update createPhoneNum.js

--- 6ku/createPhoneNum.js
+++ 6ku/createPhoneNum.js
@@ -3,16 +3,12 @@
 // Example
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 
-// Initial solution
-function createPhoneNumber(numbers){
-  return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
-}
 
 // improved solution. This way i can adapt to  various phone number formats by just changing the format variable
 function createPhoneNumber(numbers){
   var format = "(xxx) xxx-xxxx";
   
-  for(var i = 0; i < numbers.length; i++)
+  for(let i = 0; i < numbers.length; i++)
   {
     format = format.replace('x', numbers[i]);
   }
