2f50dabd23234bd81de6d625e27a5a1076a67143
Dejen Tesfai <107037555+Deju35@users.noreply.github.com>
2022-11-15 15:44:39 +0200

    Update get the Middle element.js

--- 7ku/solutions/get the Middle element.js
+++ 7ku/solutions/get the Middle element.js
@@ -14,3 +14,7 @@
 // 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
 
 
+function gimme (triplet) {
+  return triplet.indexOf([...triplet].sort((a,b)=>a-b)[1])
+ 
+  }
