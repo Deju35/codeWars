76e0c1feeecf6a83cb4a2fdc69c932ce187e1cbb
Dejen Tesfai <107037555+Deju35@users.noreply.github.com>
2022-11-17 17:34:50 +0200

    Update The average length.js

--- 7ku/solutions/The average length.js
+++ 7ku/solutions/The average length.js
@@ -10,3 +10,7 @@
 // If the average length is not an integer, use Math.round().
 // The input array's length > 1
 
+function averageLength(arr) { 
+  let avg= Math.round(arr.reduce((a,b)=>a+b.length,0)/arr.length)
+  return arr.map(v=>v.length===avg?v:v[0].repeat(avg))
+}
